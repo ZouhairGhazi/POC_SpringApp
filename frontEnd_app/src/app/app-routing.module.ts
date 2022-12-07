cd0b0fde969ecffe5c76640446b91abb4c83d41b
Mohamed Lembarki <lembarkimohamed4@gmail.com>
2022-12-07 15:08:40 +0100

    added routing #5

--- frontEnd_app/src/app/app-routing.module.ts
+++ frontEnd_app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {SignupFormComponent} from "../../.angular/signup-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignupFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
